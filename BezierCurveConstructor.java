c05d9da15479b4e0207995007cbfc186a3ef1521
lyang36 <lyang.thu@gmail.com>
2016-01-01 15:40:19 -0500

    Create BezierCurveConstructor.java

---
+++ BezierCurveConstructor.java
@@ -0,0 +1,59 @@
+package com.yang.drawpad;
+
+import android.graphics.Path;
+import android.graphics.PointF;
+
+/**
+ * Use quadratic Bezier line to smooth a hand draw path.
+ * Created by lyang on 12/31/15.
+ */
+public class BezierCurveConstructor {
+    Path path;
+
+    PointF previousPoint;
+    int pointCounter = 0;
+
+    public BezierCurveConstructor() {
+        reset();
+    }
+
+
+    /**
+     * reset the path
+     */
+    public void reset() {
+        path = new Path();
+        pointCounter = 0;
+    }
+
+
+    public void addPoint(float x, float y) {
+
+        pointCounter ++;
+        if (pointCounter == 1) {
+            path.moveTo(x, y);
+            previousPoint = new PointF(x, y);
+            return;
+        }
+
+
+        PointF mid = new PointF((x + previousPoint.x) / 2.0f, (y + previousPoint.y) / 2.0f);
+
+        if (pointCounter < 3) {
+            path.lineTo(mid.x, mid.y);
+        } else {
+            path.quadTo(previousPoint.x, previousPoint.y, mid.x, mid.y);
+        }
+        previousPoint = new PointF(x, y);
+    }
+
+    /**
+     * construct path by points
+     *
+     * @return
+     */
+    Path constructPath() {
+        return path;
+    }
+
+}
